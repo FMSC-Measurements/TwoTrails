48f63954802f0ecbfb5565619884c7c717194274
Bryan Miller <bryancmiller@fs.fed.us>
2017-02-22 13:11:52 -0700

    consts update

--- _TwoTrails.Core/Consts.cs
+++ _TwoTrails.Core/Consts.cs
@@ -10,7 +10,7 @@ namespace TwoTrails.Core
     {
         public const String DATE_FORMAT = "M/d/yyyy h:mm:ss.SSS";
 
-        public static readonly String EmptyGuid = Guid.Empty.ToString();
+        public static readonly String EmptyGuid = "00000000-0000-0000-0000-000000000000";
         public const String FullGuid = "11111111-1111-1111-1111-111111111111";
 
         public const double MINIMUM_POINT_DIGIT_ACCURACY = 0.000001d;
