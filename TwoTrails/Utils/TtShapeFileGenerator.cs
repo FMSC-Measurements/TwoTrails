d2149a5f537874ee1194167a7e894030a99e706f
Bryan Miller <bryancmiller@fs.fed.us>
2019-03-04 13:02:45 -0700

    Fix Single Digit Zone Zero Padding

--- TwoTrails/Utils/TtShapeFileGenerator.cs
+++ TwoTrails/Utils/TtShapeFileGenerator.cs
@@ -292,7 +292,7 @@ namespace TwoTrails.Utils
 
         private static void WriteProjection(string fileName, int zone)
         {
-            string _Projection = String.Format("PROJCS[\"NAD_1983_UTM_Zone_{0}N\",GEOGCS[\"GCS_North_American_1983\",DATUM[\"D_North_American_1983\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",500000.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",{1}],PARAMETER[\"Scale_Factor\",0.9996],PARAMETER[\"Latitude_Of_Origin\",0.0],UNIT[\"Meter\",1.0],AUTHORITY[\"EPSG\",269{0}]]",
+            string _Projection = String.Format("PROJCS[\"NAD_1983_UTM_Zone_{0:D2}N\",GEOGCS[\"GCS_North_American_1983\",DATUM[\"D_North_American_1983\",SPHEROID[\"GRS_1980\",6378137.0,298.257222101]],PRIMEM[\"Greenwich\",0.0],UNIT[\"Degree\",0.0174532925199433]],PROJECTION[\"Transverse_Mercator\"],PARAMETER[\"False_Easting\",500000.0],PARAMETER[\"False_Northing\",0.0],PARAMETER[\"Central_Meridian\",{1}],PARAMETER[\"Scale_Factor\",0.9996],PARAMETER[\"Latitude_Of_Origin\",0.0],UNIT[\"Meter\",1.0],AUTHORITY[\"EPSG\",269{0}]]",
                 zone, (zone * 6 - 183));
             
             if (fileName != null && fileName.Length > 0)
