b7a771fd815265cd4276bcf3be94546c418689e9
Bryan Miller <bryan.c.miller@usda.gov>
2019-05-24 13:41:47 -0600

    Updated Reindex Options

--- TwoTrails/Dialogs/ReindexDialog.xaml.cs
+++ TwoTrails/Dialogs/ReindexDialog.xaml.cs
@@ -1,20 +1,7 @@
-﻿using FMSC.Core.Windows.Utilities;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using TwoTrails.Core;
-using TwoTrails.Core.Points;
+using TwoTrails.ViewModels;
 
 namespace TwoTrails.Dialogs
 {
@@ -23,62 +10,10 @@ namespace TwoTrails.Dialogs
     /// </summary>
     public partial class ReindexDialog : Window
     {
-        private TtHistoryManager _Manager;
-        public ReadOnlyObservableCollection<TtPolygon> Polygons => _Manager.Polygons;
-        public TtPolygon SelectedPolygon { get; set; }
-
-
         public ReindexDialog(TtHistoryManager manager)
         {
-            _Manager = manager;
-            this.DataContext = this;
+            this.DataContext = new ReindexModel(this, manager);
             InitializeComponent();
-        }
-
-
-        private void Reindex(bool byPid)
-        {
-            if (SelectedPolygon != null)
-            {
-                List<TtPoint> points = _Manager.GetPoints(SelectedPolygon.CN);
-
-                if (points.Count > 0)
-                {
-                    if (byPid)
-                    {
-                        _Manager.EditPointsMultiValues(points.OrderBy(p => p.PID), PointProperties.INDEX, Enumerable.Range(0, points.Count));
-                    }
-                    else
-                    {
-                        _Manager.EditPointsMultiValues(points, PointProperties.INDEX, Enumerable.Range(0, points.Count));
-                    }
-
-
-                    if (this.IsShownAsDialog())
-                        this.DialogResult = true;
-                    Close();
-                }
-                else
-                {
-                    MessageBox.Show("Polygon does not contain any points.");
-                }
-            }
-            else
-            {
-                MessageBox.Show("No Polygon selected.");
-            }
-        }
-
-
-
-        private void CO_Click(object sender, RoutedEventArgs e)
-        {
-            Reindex(false);
-        }
-
-        private void PID_Click(object sender, RoutedEventArgs e)
-        {
-            Reindex(true);
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
---
+++ TwoTrails/ViewModels/ReindexModel.cs
@@ -0,0 +1,82 @@
+﻿using CSUtil.ComponentModel;
+using FMSC.Core.Windows.ComponentModel.Commands;
+using FMSC.Core.Windows.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using TwoTrails.Core;
+using TwoTrails.Core.Points;
+
+namespace TwoTrails.ViewModels
+{
+    public class ReindexModel : NotifyPropertyChangedEx
+    {
+        private TtHistoryManager _Manager;
+        private Window _Window;
+        public ReadOnlyObservableCollection<TtPolygon> Polygons => _Manager.Polygons;
+        public TtPolygon SelectedPolygon { get { return Get<TtPolygon>(); } set { Set(value); } }
+        public ReindexMode ReindexMode { get { return Get<ReindexMode>(); } set { Set(value); } }
+
+        public ICommand ReindexCommand { get; }
+
+
+        public ReindexModel(Window window, TtHistoryManager manager)
+        {
+            _Window = window;
+            _Manager = manager;
+            ReindexCommand = new BindedRelayCommand<ReindexModel>(x => Reindex(), x => SelectedPolygon != null, this, x => new { x.SelectedPolygon, x.ReindexMode });
+        }
+
+
+        private void Reindex()
+        {
+            if (SelectedPolygon != null)
+            {
+                List<TtPoint> points = _Manager.GetPoints(SelectedPolygon.CN);
+
+                if (points.Count > 0)
+                {
+                    if (ReindexMode == ReindexMode.PID)
+                    {
+                        _Manager.EditPointsMultiValues(points.OrderBy(p => p.PID), PointProperties.INDEX, Enumerable.Range(0, points.Count));
+                    }
+                    else if (ReindexMode == ReindexMode.CreationTime)
+                    {
+                        _Manager.EditPointsMultiValues(points.OrderBy(p => p.TimeCreated), PointProperties.INDEX, Enumerable.Range(0, points.Count));
+                    }
+                    else if (ReindexMode == ReindexMode.CurrentOrder)
+                    {
+                        _Manager.EditPointsMultiValues(points, PointProperties.INDEX, Enumerable.Range(0, points.Count));
+                    }
+
+
+                    if (_Window.IsShownAsDialog())
+                        _Window.DialogResult = true;
+                    _Window.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Polygon does not contain any points.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("No Polygon selected.");
+            }
+        }
+    }
+
+    public enum ReindexMode
+    {
+        [Description("PID (Point ID)")]
+        PID = 0,
+        [Description("Creation Time")]
+        CreationTime = 1,
+        [Description("Current Order")]
+        CurrentOrder = 2
+    }
+}
