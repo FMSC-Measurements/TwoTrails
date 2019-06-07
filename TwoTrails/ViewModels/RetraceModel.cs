4c7dd4f2e2a2d2de0382a7dd5d48f307c2aae575
Bryan Miller <bryan.c.miller@usda.gov>
2019-06-07 17:11:56 -0600

    UI Updates

--- TwoTrails/ViewModels/RetraceModel.cs
+++ TwoTrails/ViewModels/RetraceModel.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reflection;
 using System.Windows;
 using System.Windows.Input;
 using TwoTrails.Core;
@@ -147,9 +146,7 @@ namespace TwoTrails.ViewModels
                             }
                         }
                     }
-
                     
-
                     if (MovePoints)
                     {
                         bool mcStarted = false;
